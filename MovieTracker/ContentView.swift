756ba0646d516a19a59eb8c169b636f7eb98fbaa
Darrell Payne <darrellpayne@earthlink.net>
2019-06-20 18:03:34 -0700

    Initial Commit

---
+++ MovieTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MovieTracker
+//
+//  Created by DARRELL A PAYNE on 6/20/19.
+//  Copyright © 2019 DARRELL A PAYNE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
